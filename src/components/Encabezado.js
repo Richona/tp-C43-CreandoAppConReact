a3d5957016397e9494ccf863ac42384f6b61e336
Richona <105828822+Richona@users.noreply.github.com>
2022-12-01 16:07:06 -0300

    microdesafio 3 completado

--- src/components/Encabezado.js
+++ src/components/Encabezado.js
@@ -3,20 +3,18 @@ import logo from '../assets/img/logo-DH.png';
 
 export default function Encabezado() {
     return (
-        <div>
-            <header class="encabezado">
-                <nav id="logo">
-                    <a href="#img"><img src={logo} alt="Logo Digital House"/></a>
-                </nav>
-                <nav id="opciones">
-                    <ul>
-                        <li><a href="#inicio">Inicio</a></li>
-                        <li><a href="#nosotros">Nosotros</a></li>
-                        <li><a href="#productos">Productos</a></li>
-                        <li><a href="#contacto">Contacto</a></li>
-                    </ul>
-                </nav>
-            </header>
-        </div>
+        <header className="encabezado">
+            <nav id="logo">
+                <a href="#img"><img src={logo} alt="Logo Digital House" /></a>
+            </nav>
+            <nav id="opciones">
+                <ul>
+                    <li><a href="#inicio">Inicio</a></li>
+                    <li><a href="#nosotros">Nosotros</a></li>
+                    <li><a href="#productos">Productos</a></li>
+                    <li><a href="#contacto">Contacto</a></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
